1403dc73f44db94e969995f9576cdbc7b29759c5
ashishkumarui6 <ashishkumarui6@gmail.com>
2025-05-01 15:33:59 +0530

    error solve

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
-const express = require("express")
-const cors = require("cors")
-require("dotenv").config()
-const connectDB = require("./App/config/connectDB")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./App/config/connectDB");
+const router = require("./App/routes/index");
+const cookieParser = require("cookie-parser");
+const { app, server } = require("./App/socket/index");
 
-const router = require("./App/routes/index")
-
-const cookieParser = require("cookie-parser")
-
-const { app, server } = require("./App/socket/index")
-
-
-// const app = express()
-
-
+// Middleware setup
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
-}))
+}));
+app.use(express.json());
+app.use(cookieParser());
 
-
-app.use(express.json())
-
-app.use(cookieParser())
-
+// Routes
+app.use("/api", router);
 
 const PORT = process.env.PORT || 5173;
 
-
-app.use("/api", router)
-
-
-
-
+// Database connection and server start
 connectDB().then(() => {
-    server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-})
-
+    server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+});
